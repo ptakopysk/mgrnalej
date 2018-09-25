41a23c8e42365837a41f35506bba5f688fc4ec4d
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2018-09-25 12:56:25 +0200

    fix kbest

--- kbest.cpp
+++ kbest.cpp
@@ -12,13 +12,27 @@ int main(){
     for (int i = 0; i < K; i++) {
         kbest[i] = -1;
     }
-    for(int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++) {
         cin >> number;
+        // see if number is bigger than sth we have
+        bool keep = false;
         for (int j = 0; j < K; j++) {
             if (kbest[j] < number) {
-                kbest[j] = number;
+                keep = true;
                 break;
             }
+        }
+        if (keep) {
+            // replace minimum with number
+            int min = 40000;
+            int mini = -1;
+            for (int j = 0; j < K; j++) {
+                if (kbest[j] < min) {
+                    min = kbest[j];
+                    mini = j;
+                }
+            }
+            kbest[mini] = number;
         }
     }
     for (int i = 0; i < K; i++) {
